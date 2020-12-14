96031bb1dd1cc269ccdcaff3bbc2f3c323034d4c
ynsdz <yunusemredize@gmail.com>
2020-12-14 14:40:28 +0300

    added forEach

--- javascript/main.js
+++ javascript/main.js
@@ -20,11 +20,11 @@ SC.get('/tracks', {
 });
 }
 
-SoundCloudAPI.getTrack("Rilo Kiley");
+SoundCloudAPI.getTrack("Deus Ex Machine İsmet Özel");
 
 SoundCloudAPI.renderTracks= function(tracks){
-
- //card 
+tracks.forEach(function(track){
+   //card 
 var card =document.createElement("div");
 card.classList.add("card");
 
@@ -34,7 +34,7 @@ imageDiv.classList.add("image");
 
 var image_img = document.createElement("img");
 image_img.classList.add("image_img");
-image_img.src = "http://www.placekitten.com/290/290";
+image_img.src = track.artwork_url ||'http://lorempixel.com/100/100/abstract/' ;
 
 imageDiv.appendChild(image_img);
 
@@ -44,7 +44,7 @@ var content= document.createElement('div');
 
 var header = document.createElement('div');
 header.classList.add('header');
-header.innerHTML= "<a href='#' target='_blank'>Science Vs. Romance</a>"
+header.innerHTML= '<a href="'+ track.permalink_url+ '"target="_blank">' + track.title + '</a>' 
     
 //button
 var button = document.createElement('div');
@@ -65,6 +65,8 @@ card.appendChild(button);
 var searchResult = document.querySelector(" .js-search-results" );
 
 searchResult.appendChild(card);
+})
+
 
                    };
 
