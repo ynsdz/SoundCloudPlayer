4e4f499304b1b00e271a679d4a29751e9295f1bb
ynsdz <yunusemredize@gmail.com>
2020-12-15 15:22:29 +0300

    added delete part and fixed search engine

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,48 @@
 /* 1.Search */
+ function sil(){
+    var removeAll= document.getElementById('button-2');
+removeAll.addEventListener('click',function(){
+    localStorage.clear();
+    location.reload();
+//    $( ".js-playlist" ).load(window.location.href + " .js-playlist" );
 
+});
+
+var removeLast= document.getElementById('button-1');
+removeLast.addEventListener('click', function(){
+var playlist = document.querySelector(".js-playlist");
+localStorage.removeItem("key");
+playlist.removeChild(playlist.firstChild);
+});
+}
+
+var UI={}
+
+UI.submit=function(){
+    var search = document.querySelector('.js-submit');
+    search.addEventListener('click',function(){
+      var container = document.querySelector(".js-search-results");
+      container.innerHTML = " ";
+    var input = document.querySelector("input").value;
+        SoundCloudAPI.getTrack(input);
+
+    });
+}
+
+UI.enter= function(){
+inputId = document.getElementById('input');
+inputId.addEventListener('keyup', function onEvent(e) {
+    if (e.keyCode === 13) {
+      var container = document.querySelector(".js-search-results");
+      container.innerHTML = " ";
+    var input = document.querySelector("input").value;
+        SoundCloudAPI.getTrack(input);
+    }
+});
+}
+
+UI.submit();
+UI.enter();
 
 /*2. Query Soundcloud API */
 var SoundCloudAPI ={}
@@ -20,7 +63,7 @@ SC.get('/tracks', {
 });
 }
 
-SoundCloudAPI.getTrack("Deus Ex Machine İsmet Özel");
+//SoundCloudAPI.getTrack("Deus Ex Machine İsmet Özel");
 
 SoundCloudAPI.renderTracks= function(tracks){
 tracks.forEach(function(track){
@@ -54,7 +97,9 @@ icon.classList.add('add','icon');
 var buttonText = document.createElement('span');
 buttonText.innerHTML='Add to playlist';
     
-    
+button.addEventListener('click',function(){
+   SoundCloudAPI.getEmbed(track.permalink_url);
+});   
 content.appendChild(header);
 button.appendChild(icon);
 button.appendChild(buttonText);
@@ -65,12 +110,23 @@ card.appendChild(button);
 var searchResult = document.querySelector(" .js-search-results" );
 
 searchResult.appendChild(card);
-})
+}) }
 
+SoundCloudAPI.getEmbed =function(trackURL){
+    SC.oEmbed(trackURL, {
+  auto_play: true
+}).then(function(embed){
+  console.log('oEmbed response: ', embed);
+    var SideBar = document.querySelector(".js-playlist");
+    var box= document.createElement('div');
+    box.innerHTML = embed.html;
+    SideBar.insertBefore(box, SideBar.firstChild);
+    localStorage.setItem("key", SideBar.innerHTML);
+});
 
-                   };
+   
+}
 
-
-/*3. Display to cards */
-
-
+var sideBar =document.querySelector( '.js-playlist' );
+sideBar.innerHTML=localStorage.getItem("key");
+sil();
